0176fe900275e8f6d82ad306fd96bf386e287208
shravanikamutala <shravani.kamutala@gmail.com>
2018-06-17 20:31:24 +0100

    second commit

--- src/test/java/com/pressassociation/automation/driver/AppResponse.java
+++ src/test/java/com/pressassociation/automation/driver/AppResponse.java
@@ -5,6 +5,7 @@ import org.apache.http.HttpStatus;
 public class AppResponse {
     int httpStatus = HttpStatus.SC_OK;
     String jsonBody = "";
+    String test;
 
     AppResponse(int httpStatus, String jsonBody) {
         this.httpStatus = httpStatus;
